6c4e24aedb5508d7ceffbc004638daad423f5e8e
mishastr77 <mishastr.1984@mail.ru>
2021-03-14 08:58:44 +0100

    дз 8 недоделаная
    нужно доделать карусель

---
+++ js/index.js
@@ -0,0 +1,74 @@
+import cards from "./gallery-item.js";
+
+const galleryRef = document.querySelector(".gallery");
+const divModalRef = document.querySelector(".lightbox");
+const modalImgRef = document.querySelector("img.lightbox__image");
+
+const dataSourceRef = cards.map(({ original }) => {
+  const dataForSlider = original;
+  return dataForSlider;
+});
+
+const mark = cards.map(({ preview, original, description }) => {
+  const itemListRef = document.createElement("li");
+  itemListRef.classList.add("gallery__item");
+  const linkRef = document.createElement("a");
+  linkRef.classList.add("gallery__link");
+  const imageRef = document.createElement("img");
+  imageRef.classList.add("gallery__image");
+  imageRef.src = preview;
+  imageRef.dataset.source = original;
+  imageRef.alt = description;
+  linkRef.append(imageRef);
+  itemListRef.append(linkRef);
+  console.log(itemListRef);
+  return itemListRef;
+});
+
+galleryRef.append(...mark);
+
+let activeIndex = 0;
+
+galleryRef.addEventListener("click", (e) => {
+  if (e.target.nodeName === "IMG") {
+    divModalRef.classList.add("is-open");
+    modalImgRef.src = e.target.dataset.source;
+    modalImgRef.alt = e.target.alt;
+    activeIndex = dataSourceRef.indexOf(modalImgRef.src);
+    window.addEventListener("click", (e) => {
+      if (
+        e.target.dataset.action === "close-lightbox" ||
+        e.target.className === "lightbox__overlay"
+      ) {
+        closeModalAndClean();
+      }
+    });
+    
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        closeModalAndClean();
+      }
+      
+
+      if (e.key === "ArrowLeft") {
+        
+        
+        if(activeIndex = 0) {
+          activeIndex = dataSourceRef.length - 1;
+          }
+        activeIndex -= 1;
+        modalImgRef.src = dataSourceRef[activeIndex];
+      }
+      if (e.key === "ArrowRight" && activeIndex < dataSourceRef.length - 1) {
+        activeIndex += 1;
+        modalImgRef.src = dataSourceRef[activeIndex];
+      }
+    });
+  }
+});
+
+const closeModalAndClean = function () {
+  divModalRef.classList.remove("is-open");
+  modalImgRef.src = "";
+  modalImgRef.alt = "";
+};
